8c0d8e72f40327f23b3de653c9c3a05fd8bbb5ec
suhass204 <classmemes5520@gmail.com>
2025-02-11 11:42:13 +0530

    Add files via upload

---
+++ voters/inc/footer.php
@@ -0,0 +1,16 @@
+
+<div class="row bg-black text-white">
+            <div class="col-12 my-3">
+                <p style="text-align: center;"> &copy; Copyright 2024 - All rights reserved <br />
+                Developed by Suhas-Vidya
+            </p>
+            </div>
+        </div>
+    </div>
+
+    
+<script src="../assert/js/jquerry.min.js"></script>
+<script src="../assert/js/bootstrap.min.js"></script>
+
+</body>
+</html>
